db2eb569306ef18f45567911da6dac6165374f74
Maximo <Maximopupo96@gmail.com>
2024-01-10 19:22:24 -0500

    Try to netflify this to see if video works, weird it dosent

---
+++ weather_app/static/script.js
@@ -0,0 +1,10 @@
+// Function to toggle between light and dark mode
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+
+// Check for user preference and set initial mode
+
+
+// Toggle dark mode on button click
+document.getElementById('dark-mode-toggle').addEventListener('click', toggleDarkMode);
