8478be5a3060c2df3ab95a9be672a142f9726d29
Maximo <Maximopupo96@gmail.com>
2024-01-12 17:54:39 -0500

    Added Current Temperature

--- weather_app/views.py
+++ weather_app/views.py
@@ -19,6 +19,9 @@ def index(request):
         else:
             weather_data2, daily_forecasts2 = None, None
 
+        if (weather_data1 is None):
+            error_message = 'Error fetching weather data. Please try again later.'
+            return render(request, 'weather_app/index.html', {'error_message': error_message})
         context = {
             'weather_data1': weather_data1,
             'daily_forecasts1': daily_forecasts1,
@@ -26,16 +29,21 @@ def index(request):
             'daily_forecasts2': daily_forecasts2,
         }
 
+
         return render(request, 'weather_app/index.html', context)
     else:
         return render(request, 'weather_app/index.html')
 
 
+
+
 def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
     response = requests.get(current_weather_url.format(city, api_key)).json()
-    lat, lon = response['coord']['lat'], response['coord']['lon']
-    forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
 
+    lat, lon = response['coord']['lat'], response['coord']['lon']
+    if(lat is None and lon is None):
+        return None, None
+    forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
     weather_data = {
         'city': city,
         'temperature': round((response['main']['temp'] -273.15) * 9/5 + 32, 2),
@@ -48,9 +56,13 @@ def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url)
         daily_forecasts.append({
             'day': datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%A'),
             'min_temp': round((daily_data['temp']['min']-273.15) * 9/5 + 32, 2),
+            'min_temp2': round((daily_data['temp']['min'] - 273.15), 2),
             'max_temp': round((daily_data['temp']['max']-273.15) * 9/5 + 32,2),
+            'max_temp2': round((daily_data['temp']['max'] - 273.15), 2),
+            'current_temp': round((daily_data['temp']['day'] - 273.15) * 9 / 5 + 32, 2),
             'description': daily_data['weather'][0]['description'],
             'icon': daily_data['weather'][0]['icon'],
         })
+
 
     return weather_data, daily_forecasts
